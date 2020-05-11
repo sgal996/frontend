1ed0d62b36a338c4f41f7cc97ab295f329bf38a8
sgal996 <galacsim@gmail.com>
2020-05-11 11:35:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Naslovna from "./components/Naslovna";
+import Login from "./components/Login";
+import Navigation from "./components/Navigation"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Muskarci from "./components/Muskarci";
+import Zene from "./components/Zene";
+import Djeca from "./components/Djeca";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+
+import {BrowserRouter as Router, Switch, Routes, Route, Link, Outlet} from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+class App extends Component {
+    render() {
+        return (
+            <main>
+                <Header></Header>
+                <Navigation></Navigation>
+                <Switch>
+                    <Route path="/" component={Muskarci} exact />
+                    <Route path="/about" component={About} />
+                    <Route path="/zene" component={Zene} />
+                    <Route path="/djeca" component={Djeca}></Route>
+                    <Route path="/kontakt" component={Contact}></Route>
+                    <Route component={NotFound} />
+                </Switch>
+                <Footer></Footer>
+            </main>
+        )
+    }
 }
 
 export default App;
