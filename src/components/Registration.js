335857e66379b62b3c84571b24eb8b8d95a67975
sgal996 <galacsim@gmail.com>
2020-05-11 19:41:17 +0200

    added some pages to fill, still to fix routes

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,6 +2,18 @@ import React, {Component} from "react";
 
 class Registration extends Component{
     render() {
-        return super.render();
+        return(
+        <div>
+            <form>
+                <label>Username:</label> <input type={<textarea name="username" id="username" cols="30" rows="10"></textarea>}>
+            </input><br/>
+                <label>Password:</label> <input type={<textarea name="password" id="password" cols="30" rows="10"></textarea>}>
+            </input><br/>
+                <label>Repeat Password:</label> <input type={<textarea name="password" id="password" cols="30" rows="10"></textarea>}>
+            </input><br/>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+        )
     }
 }
