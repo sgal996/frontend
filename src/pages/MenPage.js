4830777b375674c7d8a4d9456305bc03d37869c5
sgal996 <galacsim@gmail.com>
2020-05-19 15:45:57 +0200

    got the user

--- src/pages/MenPage.js
+++ src/pages/MenPage.js
@@ -1,12 +1,41 @@
 import React, {Component} from "react";
 
+import {currentUser} from "../redux/services/dashboard.service";
+import {connect} from "react-redux";
+
+
+
 class MenPage extends Component {
-render() {
-    return (
-        <div>Men Page sth</div>
-    );
+    componentDidMount() {
+        const {currentUser} = this.props
+        currentUser();
+        console.log({currentUser})
+    }
+
+    render() {
+
+
+        return (
+            <div>Men Page sth </div>
+        );
+    }
 }
 
-}
+    const mapStateToProps = state => {
+        return {
+            user: state.dashboard.user
 
-export default MenPage;
+        };
+    };
+    const mapDispatchToProps = dispatch => {
+        return {
+            currentUser: () => dispatch(currentUser())
+        };
+    };
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(MenPage)
+
